3667a50ed18e8a10be25219ee777ef636fdd5ad5
Andres <andybarrera782@gmail.com>
2020-10-23 15:55:35 -0700

    Github Test Commit

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
+//This program was made to test github sync
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World, this is a test";
+    std::cout << "Hello World";
     return 0;
 }
